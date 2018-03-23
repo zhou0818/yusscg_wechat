d62b4cf665beda8d9a2d5952ecd8bab9a90c8967
zhou0818 <95430589@qq.com>
2018-03-23 11:44:53 +0800

    创建项目

---
+++ src/pages/index.vue
@@ -0,0 +1,19 @@
+<template>
+    <base-page type="index-list" title="IndexList" canBack=false>
+        <div slot="content">
+            <cube-button>Default</cube-button>
+        </div>
+    </base-page>
+</template>
+
+<script type="text/ecmascript-6">
+import BasePage from '@/components/BasePage.vue'
+export default {
+  components: {
+    BasePage
+  }
+}
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus" scoped>
+</style>
